c309b5ea04dbb3867cb112cc046a9c171cd44630
Chandanpreet-Kaur-15 <5141chand@gmail.com>
2024-08-15 14:03:40 -0700

    Backend Setup done

--- Backend/server.js
+++ Backend/server.js
@@ -1,20 +1,15 @@
 const express = require("express");
 const app = express();
 
-const server = require("http").createServer(app);
-const {Server} = require("socket.io");
-
-const io = new Server(server);
+const http = require("http").createServer(app);
+const server = require("socket.io")(http);
 
 
-// Routes
+
 app.get("/", (req, res) => {
     res.send("This is a realtime whiteboard sharing web application");
 });
 
-io.on("Connection: ", (socket) => {
-    console.log("User Connected");
-});
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log("Server is running on http://localhost:5000"));
